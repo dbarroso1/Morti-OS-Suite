58087f8061d20f6d288248ea54bce81af3c7a23d
Dilean Barroso <dileanbarroso@gmail.com>
2018-04-18 17:22:59 -0400

    Integrated Angular with Django Succesfully
    needs Web API

--- Client/src/app/sidebar/sidebar.component.ts
+++ Client/src/app/sidebar/sidebar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: []
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
 
--- Server/webfront/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebfrontConfig(AppConfig):
-    name = 'webfront'

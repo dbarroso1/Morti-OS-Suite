ada53e1c4194914599a74383d056358a34364c35
Dilean Barroso <dileanbarroso@gmail.com>
2018-05-01 12:28:54 -0400

    Made Chatbot PreProcessor
    Works Perfectly.
    MortChatbot Class was made, and PreProcessor can be called from it.
    all Dictionaries are updated on Mort.py after PreProcessir Finishes
    
    need to find a way to integrate aInts2Words

--- _modules/rtc/morti.py
+++ _modules/rtc/morti.py
@@ -1,15 +1,39 @@
-"""
-Morti-OS: Suite
+import os, re, time, string
 
-"""
-import nltk
+RTC_DIR = os.path.dirname(os.path.abspath(__file__))
 
-class Morti(object):
-    def __init__(self):
-        self.name = "Morti"
-        self.questions = {}
-        self.answers = {}
+class MortiChatbot(object):
+    def __init__ (self):
+        self.id2line = {}    # Creating a Dictionary that maps each line and its ID
+        self.word2count = {} # Creating word mapped with ID's
+        self.qw2int = {}     # Creating Questions Mapped with ID's
+        self.aw2int = {}     # Creating Answers mapped with ID's # Creating and Inverse dictionery
+        self.aInts2w = {}    # Revers Dictionary of aw2Int
+        self.convo_ids = []  # Creating a list of IDs
+        self.questions = []  # Creating a Dictionary of Questions
+        self.answers = []    # Creating a Dictionary of Answers
+        self.clean_q = []    # Creating Clean Questions
+        self.clean_a = []    # Creating Clean Answers
+        self.sorted_clean_q = []
+        self.sorted_clean_a = []
+        self.threshold = 24
+        self.word_num = 0
+        
+import preprocessor as pp
+m = MortiChatbot()
+pp.buildDictionary(m.threshold,
+                    m.word_num,
+                    m.answers, 
+                    m.aw2int, 
+                    m.clean_a, 
+                    m.clean_q,
+                    m.convo_ids,
+                    m.id2line,
+                    m.questions,
+                    m.qw2int,
+                    m.sorted_clean_a,
+                    m.sorted_clean_q,
+                    m.word2count)
 
-m = Morti()
-print(m.name)
-print(m.questions)
+print("=> Sorted Clean Questions Created: {0}".format(len(m.sorted_clean_q)))
+print("=> Sorted Clean Answers Created: {0}".format(len(m.sorted_clean_a)))
--- _modules/rtc/morti_seq2seq.py
+++ _modules/rtc/morti_seq2seq.py
@@ -1,5 +1,4 @@
 import numpy as np
 import tensorflow as tf
 import re, time
-from . import morti_preprocessor as mp
 
